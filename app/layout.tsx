4fdf0bedfccf20b70d9c3c1e61c3695fa9813b3f
Lint Action <lint-action@samuelmeuli.com>
2023-11-10 15:14:34 +0000

    Fix code style issues with Prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default async function RootLayout({
   children
 }: {
   children: React.ReactNode;
-}) {  
+}) {
   return (
     <html lang="en" className="h-full bg-gray-50">
       <body className="h-full">
