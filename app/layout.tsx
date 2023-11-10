2cc2b718189167393e91ad3d6a508a7c47bcf100
John Hodkinson <john.hodkinson@softwire.com>
2023-11-10 15:12:38 +0000

    make cards cooler, allow image loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Nav from './nav';
 import Toast from './toast';
 import { Suspense } from 'react';
 import Script from 'next/script';
+import React from 'react';
 
 export const metadata = {
   title: 'Next.js 13 + PlanetScale + NextAuth + Tailwind CSS',
@@ -16,7 +17,7 @@ export default async function RootLayout({
   children
 }: {
   children: React.ReactNode;
-}) {
+}) {  
   return (
     <html lang="en" className="h-full bg-gray-50">
       <body className="h-full">
