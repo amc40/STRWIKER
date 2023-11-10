d18cd5ff7dd1fe399fa5c647a9413cc800847152
Ted While <ted.while@softwire.com>
2023-11-10 16:04:01 +0000

    Implement PlayerPoint component

--- app/components/team.tsx
+++ app/components/team.tsx
@@ -1,4 +1,5 @@
-import PlayerCard from './playercard';
+import { Player, PlayerPoint } from '@prisma/client';
+import { PlayerPointComponent } from './playerpoint';
 
 export interface TeamMember {
   id: number;
@@ -6,25 +7,27 @@ export interface TeamMember {
 }
 
 interface TeamProps {
-  teamName: string;
-  members: TeamMember[];
+  team: string;
+  playerPoints: (PlayerPoint & { player: Player })[];
 }
 
-export const Team: React.FC<TeamProps> = ({ teamName, members }) => (
-  <div
-    style={{
-      flex: 1,
-      padding: '20px',
-      border: '1px solid #ccc',
-      backgroundColor: teamName === 'Team Red' ? '#EE2E31' : '#004f98',
-      color: 'white'
-    }}
-  >
-    <h2>{teamName}</h2>
-    <ul>
-      {members.map((member) => (
-        <PlayerCard key={member.id} playerName={member.name} />
-      ))}
-    </ul>
-  </div>
-);
+export const Team: React.FC<TeamProps> = (teamProps: TeamProps) => {
+  const { team, playerPoints } = teamProps;
+  return (
+    <div
+      style={{
+        flex: 1,
+        padding: '20px',
+        border: '1px solid #ccc',
+        backgroundColor: team == 'red' ? '#EE2E31' : '#004f98',
+        color: 'white'
+      }}
+    >
+      <ul>
+        {playerPoints.map((playerPoint) => (
+          <PlayerPointComponent key={playerPoint.id} playerPoint={playerPoint} />
+        ))}
+      </ul>
+    </div>
+  );
+};
