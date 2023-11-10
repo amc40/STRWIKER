7b6857e104e2fb0592d071921d530754b7a7e287
Mike Collingwood <35658252+MichaelCollingwood@users.noreply.github.com>
2023-11-10 15:27:58 +0000

    leroyyyyyyyy jenkinnnnnnnnnsssssss (#21)
    Co-authored-by: MikCol <mike.collingwood@softwire.com>

--- app/services/gameLogicService.ts
+++ app/services/gameLogicService.ts
@@ -1,5 +1,6 @@
 import { Game } from '@prisma/client';
 import prisma from '../../lib/planetscale';
+
 export class GameLogicService {
   startGame() {
     prisma.game.create({ data: { completed: false } });
@@ -7,6 +8,65 @@ export class GameLogicService {
 
   createPlayer(name: string) {
     prisma.player.create({ data: { name } });
+  }
+
+  async reorderPlayerPoint(
+    id: number,
+    oldPosition: number,
+    newPosition: number
+  ) {
+    prisma.playerPoint.update({
+      where: {
+        id
+      },
+      data: {
+        position: newPosition
+      }
+    });
+
+    if (newPosition > oldPosition) {
+      // pushing it back
+      const playerPoints = await prisma.playerPoint.findMany({
+        where: {
+          AND: [
+            { position: { gt: oldPosition } },
+            { position: { lte: newPosition } }
+          ]
+        }
+      });
+
+      playerPoints.forEach((pp) => {
+        prisma.playerPoint.update({
+          where: {
+            id: pp.id
+          },
+          data: {
+            position: pp.position - 1
+          }
+        });
+      });
+    } else {
+      // pulling it foward
+      const playerPoints = await prisma.playerPoint.findMany({
+        where: {
+          AND: [
+            { position: { lt: oldPosition } },
+            { position: { gte: newPosition } }
+          ]
+        }
+      });
+
+      playerPoints.forEach((pp) => {
+        prisma.playerPoint.update({
+          where: {
+            id: pp.id
+          },
+          data: {
+            position: pp.position + 1
+          }
+        });
+      });
+    }
   }
 
   createPoint(currentRedScore: number, currentBlueScore: number, game: Game) {
