cdb1482bcbc7b60e2d0550650af3d93e5afe894c
John Hodkinson <john.hodkinson@softwire.com>
2023-11-10 15:53:23 +0000

    add to navbar

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -10,6 +10,7 @@ import Image from 'next/image';
 const navigation = [
   { name: 'Dashboard', href: '/' },
   { name: 'PlayerPage', href: '/playerpage' }
+  { name: 'Stats Cards', href: '/stats' }
 ];
 
 function classNames(...classes: string[]) {
