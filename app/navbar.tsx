10ae0e031b8e1abe36b656faaa698447a767bd3b
Alan Chodyniecki <alan.chody@gmail.com>
2023-12-01 15:23:38 +0000

    rename players

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -10,7 +10,7 @@ import Image from 'next/image';
 const navigation = [
   { name: 'Current Game', href: '/current-game' },
   { name: 'Stats Cards', href: '/stats' },
-  { name: 'PlayersPage', href: '/playerspage' }
+  { name: 'Players Page', href: '/players' }
 ];
 
 function classNames(...classes: string[]) {
