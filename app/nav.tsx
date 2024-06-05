55b9acbf334aaa538a3d1037d3690bc9d9d5cb7d
amc40 <36701168+amc40@users.noreply.github.com>
2024-06-05 20:59:46 +0100

    delete middleware.ts (#101)
    * delete middleware.ts
    
    * remove user from nav
    
    * remove next auth route

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import Navbar from './navbar';
-import { getServerSession } from 'next-auth/next';
 
-export default async function Nav() {
-  const session = await getServerSession();
-  return <Navbar user={session?.user} />;
-}
+const Nav: React.FC = () => {
+  return <Navbar />;
+};
+
+export default Nav;
