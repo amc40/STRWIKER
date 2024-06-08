eb4517969ea74fb98f1ada2c1df1b096a4faa678
Alan Chodyniecki <alan.chody@gmail.com>
2024-06-08 15:59:08 +0000

    fix: stop trying to be clever and just read number of games before creating one

--- app/services/gameLogicService.ts
+++ app/services/gameLogicService.ts
@@ -31,26 +31,21 @@ export class GameLogicService {
   async startGame() {
     await prisma.$transaction(
       async () => {
-        const existingGameInProgressPromise = prisma.game.findFirst({
+        const existingGameInProgress = await prisma.game.findFirst({
           where: {
             completed: false,
             abandoned: false,
           },
         });
 
-        // TODO: set rotaty dependant on number of players
-        const gamePromise = prisma.game.create({
-          data: { completed: false, rotatyBlue: 'Always', rotatyRed: 'Always' },
-        });
-
-        const [existingGameInProgress, game] = await Promise.all([
-          existingGameInProgressPromise,
-          gamePromise,
-        ]);
-
         if (existingGameInProgress != null) {
           throw new Error('Cannot create game as one is already in progress');
         }
+
+        // TODO: set rotaty dependant on number of players
+        const game = await prisma.game.create({
+          data: { completed: false, rotatyBlue: 'Always', rotatyRed: 'Always' },
+        });
 
         const initialPoint = await this.createPoint(0, 0, game);
         await prisma.game.update({
