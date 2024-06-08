0331dc0fba75c9e38445331359f42683c5f81c7d
amc40 <36701168+amc40@users.noreply.github.com>
2024-06-08 17:49:11 +0100

    center button in NoGameInProgress (#112)

--- app/no-game-in-progress/NoGameInProgress.tsx
+++ app/no-game-in-progress/NoGameInProgress.tsx
@@ -3,13 +3,11 @@ import { StartGameButton } from '../components/StartGameButton';
 
 export const NoGameInProgress: React.FC = () => {
   return (
-    <main className="flex-1 flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-          No Game in Progress
-        </h1>
-        <StartGameButton />
-      </div>
+    <main className="flex-1 flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+        No Game in Progress
+      </h1>
+      <StartGameButton />
     </main>
   );
 };
