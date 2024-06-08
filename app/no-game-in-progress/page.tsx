d20bec5011644bea038c1d927161262cfd7d3ad2
Alan Chodyniecki <alan.chody@gmail.com>
2024-06-08 15:01:57 +0000

    revalidate no game in progress and current game pages every 10s

--- app/no-game-in-progress/page.tsx
+++ app/no-game-in-progress/page.tsx
@@ -5,6 +5,8 @@ import { GameInfoService } from '../services/gameInfoService';
 
 const gameInfoService = new GameInfoService();
 
+export const revalidate = 10;
+
 export default async function Page() {
   const gameInProgress = await gameInfoService.isGameInProgress();
 
