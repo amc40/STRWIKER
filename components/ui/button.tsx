5bdd358c05289b09f6f60ffa48ac3b5bc46135b5
Alan Chodyniecki <alan.chody@gmail.com>
2025-03-01 23:33:49 +0000

    refactor RotationStrategyStatus to use a badge 'shape'

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -6,7 +6,7 @@ import { cn } from '@/lib/utils';
 import { Spinner } from '@/app/components/icons/Spinner';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0',
+  'inline-flex items-center justify-center whitespace-nowrap font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0',
   {
     variants: {
       variant: {
@@ -19,20 +19,25 @@ const buttonVariants = cva(
         outline:
           'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
         secondary:
-          'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
+          'bg-secondary text-secondary-foreground border border-black shadow-sm hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'h-9 px-4 py-2 [&_svg]:size-4',
-        sm: 'h-8 rounded-md px-3 text-sm [&_svg]:size-4',
-        lg: 'h-10 rounded-md px-8 py-2 text-md md:text-2xl font-bold [&_svg]:size-4',
-        icon: 'h-9 w-9 [&_svg]:size-5',
+        default: 'h-9 px-4 py-2 text-sm gap-2 rounded-md [&_svg]:size-4',
+        sm: 'h-8 px-3 text-sm gap-2 rounded-md [&_svg]:size-4',
+        lg: 'h-10 px-8 py-2 text-md md:text-2xl gap-2 rounded-md font-bold [&_svg]:size-4',
+        icon: 'h-9 w-9 rounded-md [&_svg]:size-5',
+      },
+      shape: {
+        default: '',
+        badge: 'rounded-full font-normal text-base px-2 h-auto py-0.5 gap-0.5',
       },
     },
     defaultVariants: {
       variant: 'default',
       size: 'default',
+      shape: 'default',
     },
   },
 );
@@ -46,13 +51,21 @@ export interface ButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   (
-    { className, variant, size, asChild = false, loading = false, ...props },
+    {
+      className,
+      variant,
+      size,
+      shape,
+      asChild = false,
+      loading = false,
+      ...props
+    },
     ref,
   ) => {
     const Comp = asChild ? Slot : 'button';
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, size, shape, className }))}
         ref={ref}
         {...props}
         disabled={loading || props.disabled}
