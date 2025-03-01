98966ce8e3e07d252bdbb293a672077e5bfe645b
Alan Chodyniecki <alan.chody@gmail.com>
2025-03-01 23:15:36 +0000

    refactor CircleRemove to use destructive-outline

--- app/components/player-card/CircleButton.tsx
+++ app/components/player-card/CircleButton.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import { Button } from '@/components/ui/button';
+import { Button, type ButtonProps } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 
 interface CircleButtonProps {
   className?: string;
   onClick: () => void;
+  variant?: ButtonProps['variant'];
 }
 
 export const CircleButton: React.FC<
   React.PropsWithChildren<CircleButtonProps>
-> = ({ children, className, onClick }) => {
+> = ({ children, className, onClick, variant = 'outline' }) => {
   return (
     <Button
       onClick={onClick}
       size="icon"
-      variant="outline"
+      variant={variant}
       className={cn('rounded-full', className)}
     >
       {children}
