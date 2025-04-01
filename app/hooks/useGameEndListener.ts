63eb1e73679a636804db7ca9c73c209f46235041
Alan Chodyniecki <alan.chody@gmail.com>
2025-04-01 13:59:14 +0000

    Add game end listener functionality and integrate with CurrentGameClient

--- app/hooks/useGameEndListener.ts
+++ app/hooks/useGameEndListener.ts
@@ -1,4 +1,6 @@
-import { useRouter } from 'next/router';
+'use client';
+
+import { useRouter } from 'next/navigation';
 import { supabaseClient } from '../utils/supabase';
 import { useCallback, useEffect } from 'react';
 
@@ -6,7 +8,7 @@ export const useGameEndListener = (gameId: number) => {
   const router = useRouter();
 
   const navigateToGameStats = useCallback(() => {
-    void router.replace(`/game/${gameId.toFixed()}/stats`);
+    router.replace(`/game/${gameId.toFixed()}/stats`);
   }, [gameId, router]);
 
   useEffect(() => {
